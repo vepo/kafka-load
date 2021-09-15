1a8eafa132cc050e213c535005ad277c91714b80
Victor Osorio <victor.osorio@openet.com>
2021-09-14 22:23:59 -0300

    Adding Step

--- parser/src/main/java/io/vepo/kafka/load/parser/TestPlan.java
+++ parser/src/main/java/io/vepo/kafka/load/parser/TestPlan.java
@@ -3,11 +3,12 @@ package io.vepo.kafka.load.parser;
 import io.vepo.kafka.load.parser.exceptions.InvalidTestPlanException;
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 public record TestPlan(String name, Connection connection, int clients, Duration cycleTime, Duration warmUp,
-                       Duration execution, Duration rampDown, List<Step> steps) {
+                       Duration execution, Duration rampDown, Step[] steps) {
     public static class TestPlanBuilder {
 
         private String name;
@@ -72,11 +73,42 @@ public record TestPlan(String name, Connection connection, int clients, Duration
 
         public TestPlan build() {
             return new TestPlan(this.name, this.connection, this.clients, this.cycleTime, this.warmUp, this.execution,
-                    this.rampDown, this.steps);
+                    this.rampDown, this.steps.toArray(Step[]::new));
         }
     }
 
     public static TestPlanBuilder builder() {
         return new TestPlanBuilder();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        TestPlan testPlan = (TestPlan) o;
+        return clients == testPlan.clients && Objects.equals(name, testPlan.name) &&
+                Objects.equals(connection, testPlan.connection) &&
+                Objects.equals(cycleTime, testPlan.cycleTime) &&
+                Objects.equals(warmUp, testPlan.warmUp) &&
+                Objects.equals(execution, testPlan.execution) &&
+                Objects.equals(rampDown, testPlan.rampDown) && Arrays.equals(steps, testPlan.steps);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(name, connection, clients, cycleTime, warmUp, execution, rampDown);
+        result = 31 * result + Arrays.hashCode(steps);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String
+                .format("TestPlan [name=%s, connection=%s, clients=%d, cycleTime=%s, warmUp=%s, execution=%s, rampDown=%s, steps=%s]",
+                        name, connection, clients, cycleTime, warmUp, execution, rampDown, Arrays.deepToString(steps));
     }
 }
